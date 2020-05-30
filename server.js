71413e848ac6d6965c40ee4e3eb7af21c91202c0
Wes (Personal) <wespersonal@Quintons-MacBook-Pro.local>
2020-05-30 03:31:00 -0500

    version 0.0.6

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const db = require('./db')
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
-const DB_USER = process.env.POSTGRES_USER;
-const DB_HOST = process.env.POSTGRES_HOST;
-const DB_DATABASE = process.env.POSTGRES_DB;
-const DB_PASSWORD = process.env.POSTGRES_PASSWORD;
+const DB_USER = process.env.PGUSER;
+const DB_HOST = process.env.PGHOST;
+const DB_DATABASE = process.env.PGDATABASE;
+const DB_PASSWORD = process.env.PGPASSWORD;
+
+const pool = new Pool()
 
 // App
 const app = express();
 app.get('/', (req, res) => {
   res.send(`Database: User: ${DB_USER}, Host: ${DB_HOST}, Database: ${DB_DATABASE}, Password: ${DB_PASSWORD}`);
+});
+
+app.get('/test', async (req, res) => {
+  const results = await db.query('SELECT NOW()')
+  res.send(JSON.stringify(results, null, 4));
 });
 
 app.listen(PORT, HOST);
